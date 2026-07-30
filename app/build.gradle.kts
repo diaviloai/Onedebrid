9b383ddb044b35c60b3480f1537a8a4fae73b317
diaviloai <diavilo@gmail.com>
2026-07-30 04:08:40 -0700

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,9 +42,11 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
+}
 
     buildFeatures {
         compose = true
