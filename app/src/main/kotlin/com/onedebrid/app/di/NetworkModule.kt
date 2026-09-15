b635986c39696b931f3cefd14d8f04ff8097ecdb
diaviloai <diavilo@gmail.com>
2026-09-14 18:58:39 -0700

    update NetworkModule.kt

--- app/src/main/kotlin/com/onedebrid/app/di/NetworkModule.kt
+++ app/src/main/kotlin/com/onedebrid/app/di/NetworkModule.kt
@@ -1,10 +1,16 @@
 package com.onedebrid.app.di
 
 import com.onedebrid.app.provider.debrid.realdebrid.RealDebridApi
+import com.onedebrid.app.provider.metadata.tmdb.TmdbApi
+import com.onedebrid.app.provider.search.torrentio.TorrentioApi
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -12,11 +18,62 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    // ... existing provider methods (e.g., Retrofit, OkHttpClient)
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        ignoreUnknownKeys = true
+        coerceInputValues = true
+        isLenient = true
+    }
 
     @Provides
     @Singleton
-    fun provideRealDebridApi(retrofit: Retrofit): RealDebridApi {
-        return retrofit.create(RealDebridApi::class.java)
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTmdbApi(
+        okHttpClient: OkHttpClient,
+        json: Json
+    ): TmdbApi {
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+            .create(TmdbApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTorrentioApi(
+        okHttpClient: OkHttpClient,
+        json: Json
+    ): TorrentioApi {
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl("https://torrentio.strem.fun/")
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+            .create(TorrentioApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRealDebridApi(
+        okHttpClient: OkHttpClient,
+        json: Json
+    ): RealDebridApi {
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl("https://api.real-debrid.com/rest/1.0/")
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory(contentType))
+            .build()
+            .create(RealDebridApi::class.java)
     }
 }
