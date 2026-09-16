5b7a92877e8b331e92e478251d29b069f559d02b
diaviloai <diavilo@gmail.com>
2026-09-16 07:01:58 -0700

    update HomeScreen.kt

--- app/src/main/kotlin/com/onedebrid/app/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/com/onedebrid/app/ui/home/HomeScreen.kt
@@ -144,7 +144,7 @@ private fun ContinueWatchingSection(
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             Text(
-                                text = item.name ?: item.mediaId,
+                                text = item.mediaId,
                                 style = MaterialTheme.typography.bodyMedium,
                                 maxLines = 1,
                                 modifier = Modifier
